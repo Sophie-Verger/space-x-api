21a41f6bc8c1caa72796842ec59159d8b6fec1c6
ophélie <ophelieleleu@yahoo.fr>
2022-04-15 17:07:05 +0200

    avec footer a terminer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>footer</h1>
+  <div>
+    <img
+      class=""
+      src="../assets/footer-picture.svg"
+      alt="Picture Space X Footer"
+      title="Picture Space X Footer"
+    
+    />
+  </div>
 </template>
 
 <style scoped>
